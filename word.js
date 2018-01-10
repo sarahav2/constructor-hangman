cc46e6a0f2e5a8cb3ac2ceb570a99bfb78d5e1e9
Sarah Avwata <sarahavwata@Sarahs-MacBook-Pro.local>
2018-01-10 17:21:07 -0500

    created a function in word.js

--- word.js
+++ word.js
@@ -1,1 +1,25 @@
 var Letter = require('./letter.js');
+
+function Word(wrd) {
+    var that = this;
+    //store the string wrd
+    this.word = wrd;
+    //collection of letter objects
+    this.letters = [];
+    this.wordFound = false;
+
+    this.getLets = function() {
+     //populate the collection above with new Letter objects
+        for(var i = 0; i<that.word.length; i++){
+          var newLetter = new Letter(that.word[i]);
+          this.letters.push(newLetter);
+        }
+      
+    };
+
+
+
+};
+
+
+module.exports = Word;
