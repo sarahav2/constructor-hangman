f725a32f25ecd1bf4cf2f56ad0973d1958220190
Sarah Avwata <sarahavwata@Sarahs-MacBook-Pro.local>
2018-01-10 18:00:45 -0500

    made changes to word and letter files

--- letter.js
+++ letter.js
@@ -4,6 +4,20 @@ var Letter = function(ltr) {
     // property/boolean if the letter can be shown
       this.appear = false;
     
+
+      this.letterRender = function() {
+        if(this.letter == ' '){ 
+          //makes sure that when the function checks if the word is found doesn't read the blank as false.
+          this.appear = true;
+          return '  ';
+        }if(this.appear === false){ 
+            /*if it doesn't appear, it returns a ' _ '*/
+          return ' _ ';
+        } else{ /*otherwise it just appears as itself*/
+          return this.letter;
+        }
+    
+      };
 };
     
     // export to use in word.js
--- word.js
+++ word.js
@@ -17,9 +17,43 @@ function Word(wrd) {
       
     };
 
+    //found the current word
+  this.didWeFindTheWord = function() {
+    if(this.letters.every(function(lttr){
+      return lttr.appear === true;
+    })){
+      this.wordFound = true;
+      return true;
+    }
 
+  };
 
-};
+  this.checkIfLetterFound = function(guessedLetter) {
+    var whatToReturn = 0;
+    //iterates through each letter to see if it matches the guessed letter
+    this.letters.forEach(function(lttr){
+      if(lttr.letter === guessedLetter){
+        lttr.appear = true;
+        whatToReturn++;
+      }
+    })
+    //if guessLetter matches Letter property, the letter object should be shown
+    return whatToReturn;
+  };
+
+  this.wordRender = function() {
+    var display = '';
+    //render the word based on if letters are found or not
+    that.letters.forEach(function(lttr){
+      var currentLetter = lttr.letterRender();
+      display+= currentLetter;
+    });
+
+    return display;
+  };
+
+}
+
 
 
 module.exports = Word;
