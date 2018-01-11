7165926a79636d3766606242002c5944c8b76309
Sarah Avwata <sarahavwata@Sarahs-MacBook-Pro.local>
2018-01-11 09:32:39 -0500

    made changes in letter.js

--- letter.js
+++ letter.js
@@ -1,7 +1,7 @@
 var Letter = function(ltr) {
     // property to store the actual letter
       this.letter = ltr;
-    // property/boolean if the letter can be shown
+    // property boolean if the letter can be shown
       this.appear = false;
     
 
